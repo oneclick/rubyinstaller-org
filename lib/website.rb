b1035872e42761855fecf5210a4fc59463eb29c1
Luis Lavena <luislavena@gmail.com>
2012-01-02 17:08:45 -0300

    Slicing and dicing the layouts
    Add some routes for testing

--- lib/website.rb
+++ lib/website.rb
@@ -1,7 +1,8 @@
 require "sinatra/base"
 
 class Website < Sinatra::Base
-  set :root,     File.expand_path("../../", __FILE__)
+  set :root,      File.expand_path("../../", __FILE__)
+  set :website,   "RubyInstaller for Windows"
 
   configure :development do
     require "sinatra/reloader"
@@ -9,6 +10,44 @@ class Website < Sinatra::Base
   end
 
   get "/" do
-    erb :index
+    erb :index, :layout => :frontpage
+  end
+
+  get "/about" do
+    section :about
+    title "About the project"
+
+    erb :about
+  end
+
+  get "/downloads" do
+    section :downloads
+    title "Downloads"
+
+    erb :releases, :locals => { :releases => [] }
+  end
+
+  get "/downloads/archives" do
+    section :downloads
+    title "Archives"
+
+    erb :releases, :locals => { :releases => [] }
+  end
+
+  def section(name = nil)
+  end
+
+  def title(value = nil)
+    @title = value if value
+    @title ? @title : ""
+  end
+
+  def page_title
+    @title ? "#{@title} ~ #{settings.website}" : settings.website
+  end
+
+  def container(value = nil)
+    @container = value if value
+    @container ? "cols-#{@container}" : "cols-sidenav"
   end
 end

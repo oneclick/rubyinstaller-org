e41bb6f8984c6a53ab1994593a9de98b3a774cd1
Luis Lavena <luislavena@gmail.com>
2012-01-07 17:29:43 -0300

    Workaround some GEM_HOME/GEM_PATH limitations
    See sinatra/sinatra-contrib#30

--- config/isolate.rb
+++ config/isolate.rb
@@ -1,5 +1,10 @@
+# do not depend on system installed gems
+options :system => false
+
 gem "sinatra", "~> 1.3.2"
 
 env :development do
+  # lock-down eventmachine to avoid errors during compilation of version 0.12.10
+  gem "eventmachine", "= 1.0.0.beta.4.1"
   gem "sinatra-contrib", "~> 1.3.1"
 end
